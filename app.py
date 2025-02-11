ce2ee9d2d72ec9838045115d759b3a6d7355cddc
JauniKappauni <social.jauni@gmail.com>
2025-02-11 20:53:38 +0100

    Added load_dotenv function from dotenv module Added os module Loaded .env file Added .gitignore file to prevent stealing api keys Added APIKEY/CHANNELID Variable to load APIKEY/CHANNELID from .env Added GET method to / Added response variable with json data from api Added data variable to process response variable and return data in json format Added channel, title, subscribers and views variables to be displayed at html website Added html pre tag to output template variables Added returning of the data variables

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 from flask import Flask, render_template
 import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
+APIKEY = os.getenv("APIKEY")
+CHANNELID = os.getenv("CHANNELID")
 
-@app.route("/", methods=["POST"])
+@app.route("/", methods=["POST", "GET"])
 def hello():
-    return render_template("index.html")
+    response = requests.get(f"https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics&id={CHANNELID}&key={APIKEY}")
+    data = response.json()
+    channel = data["items"][0]
+    title = channel["snippet"]["title"]
+    subscribers = channel["statistics"]["subscriberCount"]
+    views = channel["statistics"]["viewCount"]
+    return render_template("index.html",title=title,subscribers=subscribers,views=views)
 
 if __name__ == "__main__":
     app.run(debug=True)
