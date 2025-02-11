dc5232305477a12a6b91ef16de7b41866e90adfc
JauniKappauni <social.jauni@gmail.com>
2025-02-11 21:26:09 +0100

    Changed CHANNELID Variable to CHANNELIDS Added split method to get single ids Added empty channel_info list to contain data per channel Added for each loop to iterate through the single channels Changed return data variables to channel_info list Fixed api url Added append method to paste the data of every single channel into the channel_info list

--- app.py
+++ app.py
@@ -7,17 +7,21 @@ load_dotenv()
 
 app = Flask(__name__)
 APIKEY = os.getenv("APIKEY")
-CHANNELID = os.getenv("CHANNELID")
+CHANNELIDS = os.getenv("CHANNELIDS").split(",")  # Splitte die Channel-IDs in eine Liste
 
 @app.route("/", methods=["POST", "GET"])
 def hello():
-    response = requests.get(f"https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics&id={CHANNELID}&key={APIKEY}")
-    data = response.json()
-    channel = data["items"][0]
-    title = channel["snippet"]["title"]
-    subscribers = channel["statistics"]["subscriberCount"]
-    views = channel["statistics"]["viewCount"]
-    return render_template("index.html",title=title,subscribers=subscribers,views=views)
+    channel_info = []
+    for channel_id in CHANNELIDS:
+        response = requests.get(f"https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics&id={channel_id}&key={APIKEY}")
+        data = response.json()
+        channel = data["items"][0]
+        title = channel["snippet"]["title"]
+        subscribers = channel["statistics"]["subscriberCount"]
+        views = channel["statistics"]["viewCount"]
+        channel_info.append({"title": title, "subscribers": subscribers, "views": views})
+
+    return render_template("index.html", channel_info=channel_info)
 
 if __name__ == "__main__":
     app.run(debug=True)
